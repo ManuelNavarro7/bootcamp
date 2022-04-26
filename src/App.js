ff1841423580af2a791295116b9a48a387b2c458
ManuelNavarro7 <manuel.mnavarro@hotmail.com>
2022-04-26 10:59:02 -0300

    UseState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import './App.css';
 import Mensaje from './Mensaje';
+import { useState } from 'react';
 
-const App= ()=> {
+const App= ()=> {  
+  const MensajeContador=()=>{
+    return <h1>No se uso contador</h1>
+  }
+  let [Contador,setContador]=useState(0) 
+  let [Counter ,setCounter]=useState([])
+  const Number=({number})=>{
+      return <h1>{number}</h1>
+  }
+  const ContadorInicial = () =>{
 
+    if(Contador<10){   setContador(prevContador =>{
 
-  
-  
+       Counter.push(prevContador+1)
+       console.log(Counter)
+      return prevContador +1
+    })}
+ 
+}
+console.log(Counter)
+const ContadorReset = ({contadorI}) =>{
+ setContador(0)
+ setCounter([])
+}
+const isEven = Contador%2===0
+let mensajeParImp=isEven ? 'Es par' :'Es impar' 
   return (
     <div className="App">
-     
-   <Mensaje color='red' message='Estamos trabajando'/>
-   
-
+    <button style={{width:'80px', height:'15px'}} onClick={ContadorInicial}>Aumentar </button>
+    <button style={{width:'80px', height:'15px'}} onClick={ContadorReset}>Reset </button>
+    <Number number={Contador}/>
+    {Counter.length ===0 ? (<MensajeContador/>):( <Mensaje color='red' message='Estamos trabajando '/>)}
+    <p>{mensajeParImp}</p>
     </div>
   );
 }
--- src/Mensaje.js
+++ src/Mensaje.js
@@ -1,5 +1,7 @@
-const Mensaje = (props) =>{
-    return <h1 style={{color : props.color}}>Hola Mundo {props.message}</h1>
+const Mensaje = ({color , message}) =>{
+    //const {color} =props
+    //const {message}= props
+    return <h1 style={{color :color}}>Hola Mundo {message + color}</h1>
   }
 
   export default Mensaje
